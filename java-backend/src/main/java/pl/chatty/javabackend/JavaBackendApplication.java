8a1754c0ee21f2f6ecf16ea5abf1dd081001d3f2
machnikovsky <fraczekjakub1999@gmail.com>
2021-10-13 20:26:17 +0200

    added empty projects

---
+++ java-backend/src/main/java/pl/chatty/javabackend/JavaBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.chatty.javabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendApplication.class, args);
+    }
+
+}

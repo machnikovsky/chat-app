dc124ec178f4049a89841d9b5e680bd425f465df
CodeByJacob <kubawojtas34@gmail.com>
2021-10-23 12:21:12 +0200

    Firebase start configuration

--- java-backend/src/main/java/pl/chatty/javabackend/config/AppConfig.java
+++ java-backend/src/main/java/pl/chatty/javabackend/config/AppConfig.java
@@ -1,4 +1,22 @@
 package pl.chatty.javabackend.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class AppConfig {
+    public static void main(String[] args) throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("path/to/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://chat-app-5fd13-default-rtdb.europe-west1.firebasedatabase.app")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
 }

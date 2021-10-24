e433d5774b7795661d1756d60d1a7d0bf20012bc
machnikovsky <fraczekjakub1999@gmail.com>
2021-10-24 18:16:10 +0200

    Revert "implemented basic schema for frontend"
    This reverts commit 23fe6284781826766dafe068207e93feb826fef2.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,20 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
-import Chats from './components/Chats';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route path="/chats">
-            <Chats />
-          </Route>
-        </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 

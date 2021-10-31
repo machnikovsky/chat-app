07d26ba31a76e9554ae62bbe0638ff6c2a505a70
Diluted666 <jacoszekwojciech@gmail.com>
2021-11-01 00:02:42 +0100

    Import optimization

--- java-backend/src/main/java/controller/AdminController.java
+++ java-backend/src/main/java/controller/AdminController.java
@@ -2,7 +2,10 @@ package controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin

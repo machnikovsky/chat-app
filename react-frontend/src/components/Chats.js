bf7df8324b28004343601977801b3402ca2f78fc
weronikakleczek <weronika.kleczek@gmail.com>
2021-11-01 21:55:29 +0100

    chats page

--- react-frontend/src/components/Chats.js
+++ react-frontend/src/components/Chats.js
@@ -6,17 +6,23 @@ const Chats = () => {
             
             <div className="left-bar">
                 <div className="chats-list">
-                    <div className="single-chat">
-                        Chat 1<br/>
-                        Message 1   
+                    <div className="single-chat-container">
+                        <div className="single-chat">
+                            Chat 1<br/>
+                            Message 1   
+                        </div>
                     </div>
-                    <div className="single-chat">
-                        Chat 2<br/>
-                        Message 2   
+                    <div className="single-chat-container">
+                        <div className="single-chat">
+                            Chat 2<br/>
+                            Message 2   
+                        </div>
                     </div>
-                    <div className="single-chat">
-                        Chat 3<br/>
-                        Message 3   
+                    <div className="single-chat-container">
+                        <div className="single-chat">
+                            Chat 3<br/>
+                            Message 3   
+                        </div>
                     </div>
                 </div>
                 <div className="logout">
@@ -29,11 +35,12 @@ const Chats = () => {
                 <nav className="chat-info">
                     Chat name & info
                 </nav>
-                <div className="chatform-container">
-                    <div className="chat-form">
-                        <input type="text"/>
-                        <button type="button">Send</button>
-                    </div>
+                <div className="chat-form">
+                    <input type="text"/>
+                        Wprowadz swoja wiadomosc
+                        <div className="chat-form-line">
+                        </div>
+                        <button type="button"></button>
                 </div>
             </div>
         </div>

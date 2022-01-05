c59b8d6f8f910ed2c0fa322116d6915f2f6616e5
CodeByJacob <kubawojtas34@gmail.com>
2022-01-05 23:36:58 +0100

    Update required inputs

--- react-frontend/src/components/Register.js
+++ react-frontend/src/components/Register.js
@@ -21,6 +21,24 @@ const Register = () => {
                                 <input type="text" id="username" name="username"/>
                             </div>
                         </div>
+                        <div className="email register_div">   
+                            <label for="email">email</label>
+                            <div className="input-div">
+                                <input type="email" id="email" name="email"/>
+                            </div>
+                        </div>
+                        <div className="name register_div">   
+                            <label for="firstname">firstname</label>
+                            <div className="input-div">
+                                <input type="text" id="firstname" name="firstname"/>
+                            </div>
+                        </div>
+                        <div className="name register_div">   
+                            <label for="lastname">lastname</label>
+                            <div className="input-div">
+                                <input type="text" id="lastname" name="lastname"/>
+                            </div>
+                        </div>
                         <div className="password register_div">   
                             <label for="password">Password</label>
                             <div className="input-div">
@@ -32,8 +50,28 @@ const Register = () => {
                             <label for="password">Repeat password</label>
                             <div className="input-div">
                                 <img src={password} alt="" />
-                                <input type="password" id="repeated_password" name="password"/>
+                                <input type="password" id="repeated_password" name="repeated_password"/>
                             </div>
+                        </div>
+                        <div className="number register_div">   
+                            <label for="phone_number">phone number</label>
+                            <div className="input-div">
+                                <input type="text" id="phone_number" name="phone_number"/>
+                            </div>
+                        </div>
+                        <div className="gender register_div">
+                            <div className="input-div">
+                                <input type="radio" id="gender" name="gender" value="Male"/>
+                            </div>
+                            <label for="gender"> Male</label>
+                            <div className="input-div">
+                                <input type="radio" id="gender" name="gender" value="Famale"/>
+                            </div>
+                            <label for="gender"> Famale</label>
+                            <div className="input-div">
+                                <input type="radio" id="gender" name="gender" value="Other"/>
+                            </div>
+                            <label for="gender"> Other</label>
                         </div>
                         <Link to="/chats"><input type="image" src={button}></input></Link>
                     </form>

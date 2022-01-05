4576889cd2c4977d0b827ceb72de06f47aebb69f
Rafał Brągiel <rbragiel21@gmail.com>
2022-01-05 21:59:17 +0100

    adding mongoDB

--- java-backend/src/main/java/pl/chatty/javabackend/controller/UserController.java
+++ java-backend/src/main/java/pl/chatty/javabackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package pl.chatty.javabackend.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,13 +11,24 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
+import pl.chatty.javabackend.model.dto.request.CreateUserRequest;
+import pl.chatty.javabackend.service.UserService;
 
 import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<String> loginUser(@RequestBody Map<String, String> json){
@@ -24,8 +36,15 @@ public class UserController {
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<String> registerUser(@RequestBody Map<String, String> json){
-        return new ResponseEntity<>("Account successful registered", HttpStatus.OK);
+    public ResponseEntity<String> registerUser(@RequestBody CreateUserRequest requestBody){
+        try {
+            ResponseEntity<String> responseEntity = userService.addUser(requestBody);
+            log.info(responseEntity.getBody());
+            return responseEntity;
+        } catch (HttpClientErrorException exception) {
+            log.info(exception.toString());
+            throw new ResponseStatusException(exception.getStatusCode(), exception.getMessage());
+        }
     }
 
     @PutMapping("/{userID}/password")

18e556bece00f3cde56fcc332a4c38e24f2910b0
Diluted666 <jacoszekwojciech@gmail.com>
2022-01-27 18:59:43 +0100

    error sites routing

--- react-frontend/src/components/errors/404.js
+++ react-frontend/src/components/errors/404.js
@@ -1,15 +1,20 @@
 import Logo from "../../assets/errors/logo-black.png"
 import Image from "../../assets/errors/sad-woman.png"
 import "../../styles/errors/Error.scss"
+import {useNavigate} from "react-router-dom"
+import React from "react";
 
-function Error(){
+function Error(props){
+
+    const navigate = useNavigate();
+
      return(
          <div className="general-error-site">
              <img className="general-error-site__logo" src={Logo}/>
              <div className="general-error-site__text-block">
                  <span className="general-error-site__text-block__title">404</span>
                  <span className="general-error-site__text-block__cause">Page not found</span>
-                 <span className="general-error-site__text-block__recommendation">return to the home page</span>
+                 <span onClick={() => navigate("/")}className="general-error-site__text-block__recommendation">return to the home page</span>
              </div>
              <img className="general-error-site__image" src={Image}/>
          </div>

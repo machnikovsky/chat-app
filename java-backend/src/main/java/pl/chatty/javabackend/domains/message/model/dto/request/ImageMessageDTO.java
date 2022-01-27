3f8511c8ad88d4e6fe72f0cada03c28e330933f9
CodeByJacob <kubawojtas34@gmail.com>
2022-01-27 16:53:27 +0100

    Update DTO for message

--- java-backend/src/main/java/pl/chatty/javabackend/domains/message/model/dto/request/ImageMessageDTO.java
+++ java-backend/src/main/java/pl/chatty/javabackend/domains/message/model/dto/request/ImageMessageDTO.java
@@ -4,13 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class MessageDTO {
-    private String chatId;
-    private String messageAuthorUsername;
-    private ArrayList<String> messageReceiversUsernames;
+public class ImageMessageDTO extends MessageDTO{
+    private String imageContent;
 }

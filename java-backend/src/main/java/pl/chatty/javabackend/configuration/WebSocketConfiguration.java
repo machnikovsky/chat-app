0a5ce35b619ddd6b48cc389b35dd141d2fbd259c
rbragiel <rbragiel21@gmail.com>
2022-02-09 15:17:22 +0100

    message images

--- java-backend/src/main/java/pl/chatty/javabackend/configuration/WebSocketConfiguration.java
+++ java-backend/src/main/java/pl/chatty/javabackend/configuration/WebSocketConfiguration.java
@@ -3,14 +3,12 @@ package pl.chatty.javabackend.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @CrossOrigin(origins = "http://localhost:3000")
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -22,5 +20,12 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(5242880);
+        registration.setSendTimeLimit(20 * 10000);
+        registration.setSendBufferSizeLimit(3* 512 * 1024);
     }
 }
